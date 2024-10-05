9996565f25a1f67ea583e3152cd1bc318055083f
cigax <anc1end1eu@hotmail.com>
2024-10-04 21:58:04 -0500

    git commit and push

--- Lectura y escritura de archivo Python.py
+++ Lectura y escritura de archivo Python.py
@@ -1,12 +1,12 @@
-#La linea de codigo "with open("misdatos.txt", "w") as archivo:" crea un archivo llamado misdatos.txt
-with open("misdatos.txt", "w") as archivo:
+#La linea de codigo "with open("my_notes.txt", "w") as archivo:" crea un archivo llamado my_notes.txt
+with open("my_notes.txt", "w") as archivo:
     #Las lineas archivo.write permiten introducir caracteres al documento
-    archivo.write("Datos Personales\n")
+    archivo.write("Mis datos Personales\n")
     archivo.write("Hugo Fernandez\n")
     archivo.write("Estudiante\n")
     archivo.write("Ingenieria en las TIC\n")
 #Imprime linea por linea el texto
-with open("misdatos.txt", "r") as archivo:
+with open("my_notes.txt", "r") as archivo:
     print(archivo.readline())
     print(archivo.readline())
     print(archivo.readline())
